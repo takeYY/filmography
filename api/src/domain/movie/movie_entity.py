53b2a0a4774cb2b523660c2ccbcc18c139b49275
takeYY <t0k6yist@gmail.com>
2022-12-24 14:52:31 +0900

    🎨 ドメイン追加と変更による修正

--- api/src/domain/movie/movie_entity.py
+++ api/src/domain/movie/movie_entity.py
@@ -1,46 +1,69 @@
 from datetime import date
 
-from src.domain.movie.movie_status_enum import MovieStatus
+from src.domain.genre.genre_id_object import GenreIdObject
+from src.domain.movie.movie_id_object import MovieIdObject
+from src.domain.movie.series.movie_series_entity import MovieSeriesEntity
 from src.domain.movie.tmdb_id_object import TmdbIdObject
+from src.domain.poster.poster_object import PosterObject
 
 
 class MovieEntity:
-    """全ての映画情報を扱うエンティティ
+    """映画情報を扱うエンティティ
 
     Attributes
     ----------
-    tmdb_id: TmdbIdObject
-        TMDb ID を扱う値オブジェクト
+    movie_id: MovieIdObject
+        映画 ID を扱う値オブジェクト
     title: str
         映画タイトル
-    cover: str
-        映画カバーのURL
-    status: MovieStatus
-        鑑賞済みか否か TODO: もう少しわかりやすい名称にしたい
-    notion_id: str
-        Notion ID
+    overview: str
+        映画概要
+    poster: PosterObject
+        ポスター画像を扱う値オブジェクト
+    release_date: date
+        公開日
+    run_time: int
+        上映時間
+    series: MovieSeriesEntity
+        映画シリーズを扱うエンティティ
+    genres: list[GenreIdObject]
+        ジャンル ID を扱う値オブジェクトのリスト
+    tmdb_id: TmdbIdObject
+        TMDb ID を扱う値オブジェクト
     created_on: date
         作成日, default=date.today()
+    updated_on: date
+        更新日, default=date.today()
     """
 
     def __init__(
         self,
-        tmdb_id: TmdbIdObject,
+        movie_id: MovieIdObject,
         title: str,
-        cover: str,
-        status: MovieStatus,
-        notion_id: str,
+        overview: str,
+        poster: PosterObject,
+        release_date: date,
+        run_time: int,
+        series: MovieSeriesEntity,
+        genres: list[GenreIdObject],
+        tmdb_id: TmdbIdObject,
         created_on: date = date.today(),
+        updated_on: date = date.today(),
     ):
-        self.tmdb_id: TmdbIdObject = tmdb_id
+        self.movie_id: MovieIdObject = movie_id
         self.title: str = title
-        self.cover: str = cover
-        self.status: MovieStatus = status
-        self.notion_id: str = notion_id
+        self.overview: str = overview
+        self.poster: PosterObject = poster
+        self.release_date: date = release_date
+        self.run_time: int = run_time
+        self.series: MovieSeriesEntity = series
+        self.genres: list[GenreIdObject] = genres
+        self.tmdb_id: TmdbIdObject = tmdb_id
         self.created_on: date = created_on
+        self.updated_on: date = updated_on
 
     def __eq__(self, o: object) -> bool:
         if isinstance(o, MovieEntity):
-            return self.tmdb_id == o.tmdb_id
+            return self.movie_id == o.movie_id
 
         return False
