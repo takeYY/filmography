d24226642b1b97e0100b6315eac5989eb07e629f
takeYY <t0k6yist@gmail.com>
2023-01-15 18:20:34 +0900

    🔥 movieをドメイン, インフラ, アプリ, プレゼンから削除

--- api/src/domain/status/appreciation_status_enum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class AppreciationStatus(Enum):
-    """鑑賞状況を扱う Enum"""
-
-    WATCHED = "Watched"
-    WANNA_WATCH = "Wanna Watch"
--- api/src/infrastructure/movie/movie_repository.py
+++
@@ -1,9 +0,0 @@
-from src.domain.movie.movie_repository import IMovieRepository
-
-
-class ImplMovieRepository(IMovieRepository):
-    def __init__(self) -> None:
-        pass
-
-    def find_by_id(self, id: str):
-        return None
