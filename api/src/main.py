bd2b7b84dcb0ae7a59edac4a0e9ac51d543015ad
takeYY <t0k6yist@gmail.com>
2023-01-15 18:25:42 +0900

    ✨ 映画記録を扱うエンドポイントを追加

--- api/src/main.py
+++ api/src/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-
 from src.presentation.containers import Container
 from src.presentation.default.default_route import default_router
+from src.presentation.film_record.query.film_record_query_route import film_record_query_router
 from src.presentation.jumanpp.jumanpp_route import jumanpp_router
-from src.presentation.movie.query.movie_query_route import movie_query_router
 
 app = FastAPI()
-app.container = Container()
-
+Container()
 
 app.include_router(default_router, tags=["default"])
 app.include_router(jumanpp_router, prefix="/jumanpp", tags=["jumanpp"])
-app.include_router(movie_query_router, prefix="/movie/query", tags=["movie"])
+app.include_router(film_record_query_router, prefix="/film_record/query", tags=["film_record"])
--- api/src/presentation/containers.py
+++ api/src/presentation/containers.py
@@ -1,17 +1,22 @@
 from dependency_injector import containers
+from dependency_injector.providers import Factory
+from src.application.film_record.query.interface.film_record_query_application_interface import (
+    IFilmRecordQueryApplication,
+)
 from src.application.jumanpp.jumanpp_application import JumanppApplication
-from src.application.movie.query.movie_query_application import IMovieQueryApplication
+from src.presentation.film_record.query.film_record_query_container import FilmRecordQueryContainer
 from src.presentation.jumanpp.jumanpp_container import JumanppContainer
-from src.presentation.movie.query.movie_query_container import MovieQueryContainer
 
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         modules=[
             ".jumanpp.jumanpp_route",
-            ".movie.query.movie_query_route",
+            ".film_record.query.film_record_query_route",
         ]
     )
 
-    jumanpp_application: JumanppApplication = JumanppContainer.jumanpp_application
-    movie_query_application: IMovieQueryApplication = MovieQueryContainer.movie_query_application
+    jumanpp_application: Factory[JumanppApplication] = JumanppContainer.jumanpp_application
+    film_record_query_application: Factory[
+        IFilmRecordQueryApplication
+    ] = FilmRecordQueryContainer.film_record_query_application
