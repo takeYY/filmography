a6135e15c6bc55d2eec5357f6442ad26e6706f13
takeYY <t0k6yist@gmail.com>
2023-01-25 23:21:32 +0900

    🏷️ ドメインサービスのインターフェースを追加

---
+++ api/src/application/film_record/command/interface/film_record_command_domain_service.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from base_model.film_record.create.film_record_create_model import FilmRecordCreateModel
+from src.domain.film_record.film_record_entity import FilmRecordEntity
+
+
+class IFilmRecordCommandDomainService(ABC):
+    @abstractmethod
+    def create_film_record(self, film_record_model: FilmRecordCreateModel) -> FilmRecordEntity:
+        raise NotImplementedError
