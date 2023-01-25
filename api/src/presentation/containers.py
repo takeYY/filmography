d7558ea37057864e48fbcd9894f372ddc7dc46ee
takeYY <t0k6yist@gmail.com>
2023-01-25 23:25:23 +0900

    ✨ 映画記録作成のエンドポイントを追加

--- api/src/presentation/containers.py
+++ api/src/presentation/containers.py
@@ -1,9 +1,11 @@
 from dependency_injector import containers
 from dependency_injector.providers import Factory
+from src.application.film_record.command.interface.film_record_command_application import IFilmRecordCommandApplication
 from src.application.film_record.query.interface.film_record_query_application_interface import (
     IFilmRecordQueryApplication,
 )
 from src.application.jumanpp.jumanpp_application import JumanppApplication
+from src.presentation.film_record.command.film_record_command_container import FilmRecordCommandContainer
 from src.presentation.film_record.query.film_record_query_container import FilmRecordQueryContainer
 from src.presentation.jumanpp.jumanpp_container import JumanppContainer
 
@@ -13,6 +15,7 @@ class Container(containers.DeclarativeContainer):
         modules=[
             ".jumanpp.jumanpp_route",
             ".film_record.query.film_record_query_route",
+            ".film_record.command.film_record_command_route",
         ]
     )
 
@@ -20,3 +23,6 @@ class Container(containers.DeclarativeContainer):
     film_record_query_application: Factory[
         IFilmRecordQueryApplication
     ] = FilmRecordQueryContainer.film_record_query_application
+    film_record_command_application: Factory[
+        IFilmRecordCommandApplication
+    ] = FilmRecordCommandContainer.film_record_command_application
---
+++ api/src/presentation/film_record/command/film_record_command_route.py
@@ -0,0 +1,38 @@
+from base_model.film_record.create.film_record_create_model import FilmRecordCreateModel
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends, status
+from src.application.film_record.command.interface.film_record_command_application import IFilmRecordCommandApplication
+from src.presentation.containers import Container
+from src.utils.setup_logger import setup_logger
+
+film_record_command_router = APIRouter()
+
+
+@film_record_command_router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+)
+@inject
+async def film_record_command(
+    film_record: FilmRecordCreateModel,
+    film_record_command_application: IFilmRecordCommandApplication = Depends(
+        Provide[
+            Container.film_record_command_application,
+        ]
+    ),
+):
+    logger = setup_logger(__name__)
+    try:
+        logger.info("映画記録のコマンド: 開始")
+
+        response = await film_record_command_application.create_film_record(film_record_model=film_record)
+
+        logger.info("映画記録のクエリ: 終了")
+
+        return response
+    except Exception as e:
+        logger.error(f"映画記録のクエリでエラー. LOG: {e}", exc_info=True)
+        return dict(
+            status="error",
+            result=e,
+        )
