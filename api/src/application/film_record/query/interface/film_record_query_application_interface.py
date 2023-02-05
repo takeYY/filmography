de9bad7961b2c233cd46446114861b195fe5afdc
takeYY <t0k6yist@gmail.com>
2023-02-05 20:10:33 +0900

    🎨 コードを改善

--- api/src/application/film_record/query/interface/film_record_query_application_interface.py
+++ api/src/application/film_record/query/interface/film_record_query_application_interface.py
@@ -7,11 +7,13 @@ from src.domain.film_record.film_record_repository import IFilmRecordRepository
 class IFilmRecordQueryApplication(ABC):
     film_record_repository: IFilmRecordRepository
 
-    # NITS: 1行が長いので改行する
     @abstractmethod
-    async def fetch_film_record_by_id(self, id: str) -> dict[str, str | FilmRecordEntity | None]:
+    async def fetch_film_record_by_id(
+        self,
+        id: str,
+    ) -> dict[str, str | FilmRecordEntity | None]:
         raise NotImplementedError
 
-    # TODO: abstractmethodデコレータを追加する
+    @abstractmethod
     async def fetch_film_records(self):
         raise NotImplementedError
