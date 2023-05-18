948322d9082fc6198f350148feffb15004c57caa
takeYY <t0k6yist@gmail.com>
2023-05-18 23:08:00 +0900

    feat: default response

--- api/src/presentation/default/default_route.py
+++ api/src/presentation/default/default_route.py
@@ -1,9 +1,21 @@
+# 外部ライブラリ
 from fastapi import APIRouter
+
+# 独自ライブラリ
+from src.schemas.default import DefaultResponse
 
 default_router = APIRouter()
 
 
-@default_router.get("/")
+@default_router.get(
+    "/",
+    responses={
+        200: {
+            "description": "デフォルトの結果",
+            "model": DefaultResponse,
+        },
+    },
+)
 async def default():
     return dict(
         status="success",
---
+++ api/src/schemas/default/__init__.py
@@ -0,0 +1,1 @@
+from .response_default import DefaultResponse
