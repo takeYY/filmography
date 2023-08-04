1ade12ce515f5b04c489fa49a9a0ae927db11d74
takeYY <t0k6yist@gmail.com>
2023-08-04 23:16:05 +0900

    add: film query endpoint

--- api/src/main.py
+++ api/src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 # 独自ライブラリ
 from src.presentation import Container
 from src.presentation.default.default_route import default_router
+from src.presentation.film.query.film_query_route import film_query_router
 from src.presentation.film_record.command.film_record_command_route import film_record_command_router
 from src.presentation.film_record.query.film_record_query_route import film_record_query_router
 
@@ -12,7 +13,7 @@ from src.presentation.film_record.query.film_record_query_route import film_reco
 app = FastAPI(
     title="Filmography API",
     description="FilmographyのAPI",
-    version="0.1.1.1",
+    version="0.2.2.2",
 )
 Container()
 
@@ -20,6 +21,7 @@ app.include_router(default_router, tags=["default"])
 # app.include_router(jumanpp_router, tags=["jumanpp"])
 app.include_router(film_record_query_router, tags=["film_record"])
 app.include_router(film_record_command_router, prefix="/film_record/command", tags=["film_record"])
+app.include_router(film_query_router, tags=["film"])
 
 
 # フロントのテンプレ
